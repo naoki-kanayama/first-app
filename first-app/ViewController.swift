0a31a68917c1f0c390484c2d47310b64256919a8
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-09-22 11:26:02 +0900

    hello world

--- first-app/ViewController.swift
+++ first-app/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let text = UILabel(frame: CGRect(x: 150, y: 100, width: 300, height: 100))
+        text.text = "hello world!"
+        self.view.addSubview(text)
     }
 
 
